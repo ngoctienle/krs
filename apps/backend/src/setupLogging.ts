b809863dc59c5b3afcc7e20ae92f3b08d8c2dcc7
Tien <tien>
2023-11-11 03:08:16 +0700

    add logger

---
+++ apps/backend/src/setupLogging.ts
@@ -0,0 +1,23 @@
+import  *  as  winston  from  'winston';
+import  DailyRotateFile from 'winston-daily-rotate-file';
+
+const transport: DailyRotateFile = new DailyRotateFile({
+    filename: 'application-%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+});
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.label({ label: 'Demo anonystick:' }),
+        winston.format.timestamp(),
+        winston.format.prettyPrint(),
+      ),
+    transports: [
+    transport
+    ]}
+);
+
+export default logger;
